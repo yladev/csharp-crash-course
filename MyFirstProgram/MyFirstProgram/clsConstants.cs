cd2c56089bbf9500a0ec3b3e236a03ecf8cbab7f
yladev <yanlinnaung881@gmail.com>
2023-12-15 23:06:14 +0630

    update constants

---
+++ MyFirstProgram/MyFirstProgram/clsConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class clsConstants
+    {
+        static void Main(string[] args)
+        {
+            // constants = immutable values which are known at compile time and do not change for the life of the program
+
+            const double pi = 3.1415;
+
+            Console.WriteLine("pi value: " + pi);
+            Console.Read();
+        }
+    }
+}
