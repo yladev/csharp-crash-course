ca23813a66eecf7c68c5a6d4044592359d4678bc
yladev <yanlinnaung881@gmail.com>
2023-12-19 22:29:21 +0630

    update hypotenuse calculator program

---
+++ MyFirstProgram/MyFirstProgram/clsHypotenuseCalculatorProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class clsHypotenuseCalculatorProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter side A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter side B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt((a * a) + (b * b));
+
+            Console.WriteLine("The hypotenuse is: " + c);
+            Console.ReadKey();
+        }
+    }
+}
