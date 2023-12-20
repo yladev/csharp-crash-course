eeee896c0ee79d9c3a3c1867cca55adf2371e487
yladev <yanlinnaung881@gmail.com>
2023-12-20 21:02:55 +0630

    update string method

---
+++ MyFirstProgram/MyFirstProgram/clsStringMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class clsStringMethods
+    {
+        static void Main(string[] args)
+        {
+            String fullName = "Jason Andrew";
+            fullName = fullName.ToUpper();
+            fullName = fullName.ToLower();
+            Console.WriteLine("My name is " + fullName);
+
+            String phoneNumber = "09-254488150";
+            phoneNumber = phoneNumber.Replace("0", "9");
+            Console.WriteLine(phoneNumber);
+
+            String userName = fullName.Insert(0, "Mr.");
+            Console.WriteLine(userName);
+
+            Console.WriteLine(fullName.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
