2827e3cb2f85845ce2592e08fc42f56cb3011356
yladev <yanlinnaung881@gmail.com>
2024-01-07 23:59:20 +0630

    update while loop

--- MyFirstProgram/MyFirstProgram/clsWhileLoop.cs
+++ MyFirstProgram/MyFirstProgram/clsWhileLoop.cs
@@ -10,7 +10,8 @@ namespace MyFirstProgram
     {
         static void Main(string[] args)
         {
-                
+            Console.WriteLine("Enter your name:");
+                  
         }
     }
 }
