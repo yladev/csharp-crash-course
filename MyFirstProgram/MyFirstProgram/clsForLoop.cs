af761c52635cbbf59dcd890505b8c37a72625e4a
yladev <yanlinnaung881@gmail.com>
2024-01-11 23:48:31 +0630

    update for loop

---
+++ MyFirstProgram/MyFirstProgram/clsForLoop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class clsForLoop
+    {
+        static void Main(string[] args)
+        {
+            
+            // for loop = repeats some code a FINITE amount of times
+            for (int i = 0; i < 10; i+=1)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
