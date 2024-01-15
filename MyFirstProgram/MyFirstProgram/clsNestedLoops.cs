749d5f054ed9f6138f22e655b4fddd402394b923
yladev <yanlinnaung881@gmail.com>
2024-01-15 23:25:06 +0630

    update number guessing game

--- MyFirstProgram/MyFirstProgram/clsNestedLoops.cs
+++ MyFirstProgram/MyFirstProgram/clsNestedLoops.cs
@@ -13,7 +13,23 @@ namespace MyFirstProgram
             // nested loops == loops inside of other loops 
             //                 Uses vary. Used a lot in sorting algorithms
 
-            Console.WriteLine("How many rows?: ");
+            Console.Write("How many rows?: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How many columns?: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What symbol: ");
+            String symbol = Console.ReadLine();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
 
             Console.ReadKey();
         }
---
+++ MyFirstProgram/MyFirstProgram/clsNumberGuessingGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class clsNumberGuessingGame
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            int min = 1;
+            int max = 100;
+            int guess;
+            int number;
+            int guesses;
+
+            while (playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                number = random.Next(min, max + 1);
+
+                while (guess != number)
+                {
+                    Console.WriteLine("Guess a number between " + min + " - " + max + " : ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Guess: " + guess);
+
+                    if (guess > number)
+                    {
+                        Console.WriteLine(guess + " is to high!");
+                    }
+                    else if (guess < number)
+                    {
+                        Console.WriteLine(guess + " is to low!");
+                    }
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
