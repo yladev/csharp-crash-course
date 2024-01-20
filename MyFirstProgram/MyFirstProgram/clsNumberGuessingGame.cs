9dcde455b01544fd96fd540fdb4131f7b20c789b
yladev <yanlinnaung881@gmail.com>
2024-01-20 18:04:11 +0630

    update calculator program

--- MyFirstProgram/MyFirstProgram/clsNumberGuessingGame.cs
+++ MyFirstProgram/MyFirstProgram/clsNumberGuessingGame.cs
@@ -17,11 +17,13 @@ namespace MyFirstProgram
             int guess;
             int number;
             int guesses;
+            String response;
 
             while (playAgain)
             {
                 guess = 0;
                 guesses = 0;
+                response = "";
                 number = random.Next(min, max + 1);
 
                 while (guess != number)
@@ -32,15 +34,33 @@ namespace MyFirstProgram
 
                     if (guess > number)
                     {
-                        Console.WriteLine(guess + " is to high!");
+                        Console.WriteLine(guess + "is to high!");
                     }
                     else if (guess < number)
                     {
                         Console.WriteLine(guess + " is to low!");
                     }
+                    guesses++;
+                }
+                Console.WriteLine("Number: " + number);
+                Console.WriteLine("YOU WIN!");
+                Console.WriteLine("Guesses: " + guesses);
+
+                Console.WriteLine("Would you like to play again (Y/N): ");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+
+                if (response == "Y")
+                {
+                    playAgain = true;
+                }
+                else
+                {
+                    playAgain = false;
                 }
             }
 
+            Console.WriteLine("Thanks for playing! ... I guess");
 
             Console.ReadKey();
         }
