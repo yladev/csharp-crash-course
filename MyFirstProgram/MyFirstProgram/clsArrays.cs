1e21384babc7f941ff7f74ce87221c27ae5cd489
yladev <yanlinnaung881@gmail.com>
2024-01-21 23:08:08 +0630

    update array

---
+++ MyFirstProgram/MyFirstProgram/clsArrays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class clsArrays
+    {
+        static void Main(string[] args)
+        {
+            // array = a variable that can store multiple values. fixed size
+
+            Console.ReadKey(); 
+        }
+    }
+}
