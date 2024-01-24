19e774a00c85d8af66abdb3cc8e22feecc2e7f66
yladev <yanlinnaung881@gmail.com>
2024-01-24 23:56:46 +0630

    update method

---
+++ MyFirstProgram/MyFirstProgram/clsMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class clsMethods
+    {
+        static void Main(string[] args)
+        {
+            // method = performs a section of code, whenever it's called "invoked".
+            //          benefit = Let's us reuse code w/o writing it multiple times.
+
+
+            Console.ReadKey();
+        }
+    }
+}
